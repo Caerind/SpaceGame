1e907fc819cfaa9657d0e535a5f8ea8dd95e6635
Cmdu76 <charles.mailly@free.fr>
2020-07-25 15:24:54 +0200

    Add script + clean

--- submit.sh
+++ submit.sh
@@ -1,5 +1,3 @@
-cp build/LudumDare46-Client/Debug/LudumDare46Client.exe LD46.exe
+cp build/SpaceGame/Debug/SpaceGame.exe SpaceGame.exe
 
-cp build/LudumDare46-Client/Debug/LudumDare46Client.exe C:/Users/charl/Dropbox/LD46/Demi/LD46.exe
-rm -rf C:/Users/charl/Dropbox/LD46/Demi/Assets
-cp -r Assets C:/Users/charl/Dropbox/LD46/Demi/Assets
+cp build/SpaceGame/Debug/SpaceGame.exe "C:/Users/charl/Dropbox/Space Game/SpaceGame.exe"
